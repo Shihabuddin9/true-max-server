4ffb94b4120fa8df422b75c91f820531600a00fb
Shihabuddin9 <shihab.bd.pk@gmail.com>
2022-04-30 15:10:14 +0600

    basic server site setup

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+// middleWare
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+app.listen(port, () => {
+    console.log("success", port)
+})
