1729b9347a1151f30c0c02961f27cce29ee57d0a
Shihabuddin9 <shihab.bd.pk@gmail.com>
2022-05-03 19:48:18 +0600

    Trusted dealers incon and Body Type car module add

--- index.js
+++ index.js
@@ -22,9 +22,13 @@ async function run() {
         const BestDealsCollection = client.db("BestDeals").collection('BestDealsCar');
         // Brands Icon in Database
         const BrandsCollection = client.db("Brands").collection('BrandsIcon');
+        // Trusted Dealers company in Database
+        const TrustedDealersCollection = client.db("TrustedDealers").collection('TrustedDealersIcon');
+        // Body Type car in Database
+        const BodyTypeCarCollection = client.db("BodyType").collection('BodyTypeCar');
 
 
-        // Show Brands Icon
+        //Show Brands Icon in Home page
         app.get('/viewBrands', async (req, res) => {
             const query = {};
             const cursor = BrandsCollection.find(query);
@@ -33,13 +37,30 @@ async function run() {
         })
 
 
-        // Show Best Deals Car
+        // Show Best Deals Car in Home page
         app.get('/viewCar', async (req, res) => {
             const query = {};
             const cursor = BestDealsCollection.find(query);
             const result = await cursor.toArray();
             res.send(result)
         })
+
+        //Show Trusted Dealers Icon in Home page
+        app.get('/trustedDealers', async (req, res) => {
+            const query = {};
+            const cursor = TrustedDealersCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        // Show Body Type Car module in Home page
+        app.get('/bodyTypeCar', async (req, res) => {
+            const query = {};
+            const cursor = BodyTypeCarCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
 
 
     }
