1b26bbd36a7bd56b68a4609865d9c1ecfe60b207
Shihabuddin9 <shihab.bd.pk@gmail.com>
2022-05-03 18:24:17 +0600

    Show Best Deals Car

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
@@ -9,10 +11,30 @@ app.use(express.json())
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8ivf2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run() {
+    try {
+        await client.connect();
+        const BestDealsCollection = client.db("BestDeals").collection('BestDealsCar');
+
+        // Show Best Deals Car
+        app.get('/viewCar', async (req, res) => {
+            const query = {};
+            const cursor = BestDealsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
 
 
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 
