5627129522fae953bc044dd34b3b61f00ef93307
Shihabuddin9 <shihab.bd.pk@gmail.com>
2022-05-05 09:21:45 +0600

    Best Deals details api create

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
@@ -46,6 +46,13 @@ async function run() {
             const cursor = BestDealsCollection.find(query);
             const result = await cursor.toArray();
             res.send(result)
+        })
+        // Show Best Deals Car Details in other page
+        app.get('/viewCar/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const viewCar = await BestDealsCollection.findOne(query);
+            res.send(viewCar);
         })
 
         //Show Trusted Dealers Icon in Home page
